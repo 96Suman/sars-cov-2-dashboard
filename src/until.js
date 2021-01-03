29876ec5891bad0cd196d0d3520295ceb63061a1
96Suman <suman.gautam@city.ac.uk>
2021-01-03 17:03:33 +0000

    Added maps and circles/popup to visualized the covid-19 deaths, total number of cases and recovered cases

--- src/until.js
+++ src/until.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { Popup, Circle, CircleMarker, Tooltip } from "react-leaflet";
+
 export const sortData = (data) => {
   let sortedData = [...data];
   sortedData.sort((a, b) => {
@@ -8,4 +11,17 @@ export const sortData = (data) => {
     }
   });
   return sortedData;
+};
+
+export const showCovidDataInMap = (worldwideInfo, casesType = "cases") => {
+  console.log(worldwideInfo);
+  worldwideInfo.map((countryData) => (
+    <CircleMarker
+      center={[countryData.countryInfo.lat, countryData.countryInfo.long]}
+      pathOptions={{ color: "red" }}
+      radius={20}
+    >
+      <Tooltip>Tooltip for CircleMarker</Tooltip>
+    </CircleMarker>
+  ));
 };
